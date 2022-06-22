fad668c253fc39b5c457489f5e9ef96e81474322
347330613@qq.com <347330613@qq.com>
2022-06-22 11:23:08 +0800

    222

--- src/main/java/com/zy/com/zy/mapper/FeeBaseMapper.java
+++ src/main/java/com/zy/com/zy/mapper/FeeBaseMapper.java
@@ -3,7 +3,7 @@ package com.zy.mapper;
 import com.zy.entity.FeeBase;
 
 public interface FeeBaseMapper {
-    //测试数据相关信息
+    //测试数据相关信息333
     int deleteByPrimaryKey(String id);
 
     int insert(FeeBase record);
